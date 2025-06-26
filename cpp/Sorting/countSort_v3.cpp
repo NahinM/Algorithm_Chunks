e65cb08f4ab3551be5cb1c663edcf9949f552f33
Nahin Munkar <nahinmunkarnm@gmail.com>
2025-06-26 20:36:09 +0600

    add countSort_v3 cpp & python

---
+++ cpp/Sorting/countSort_v3.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define vi vector<int>
+
+vi count_sort(vi &arr){
+    int n = arr.size();
+    if(n<=1) return arr;
+    int MIN,MAX;
+    MIN=MAX=arr[0];
+    for(int x:arr){ // find the minimum and the maximum from the array
+        MIN=min(MIN,x);
+        MAX=max(MAX,x);
+    }
+
+    int N = MAX-MIN+1; // find the length of the 'counting range from MIN to MAX'
+    vi count(N);
+    for(int x:arr) ++count[x-MIN]; // count the frequency of the occurence
+    for(int i=1;i<N;i++) count[i]+=count[i-1]; // get the coumilitive sum
+    vi sorted_arr(n); 
+    for(int x:arr){
+        int ndx = --count[x-MIN]; // find the position of the x where it should be placed
+        sorted_arr[ndx] = x; // place the number at the correct position
+    }
+    return sorted_arr;
+}
+
+int main(){
+    freopen("input.txt","r",stdin);
+    int n;
+    cin >> n;
+    vi arr(n);
+    for(int i=0;i<n;i++) cin >> arr[i];
+    vi sorted_arr = count_sort(arr);
+    for(int x:sorted_arr) cout << x << ' ';
+    cout << '\n';
+}
---
+++ python/Sorting/countSort_v3.py
@@ -0,0 +1,26 @@
+import sys
+sys.stdin = open("input.txt","r")
+
+def count_sort(arr):
+    n = len(arr)
+    if n<=1: return arr
+    # find the minimum and the maximum from the array
+    mn = min(arr)
+    mx = max(arr)
+
+    N = mx-mn+1 # find the length of the 'counting range from MIN to MAX'
+    count = [0]*N
+    for x in arr: count[x-mn]+=1 # count the frequency of the occurence
+    for i in range(1,N): count[i]+=count[i-1] # get the coumilitive sum
+
+    sorted_arr = [0]*n
+    for x in arr:
+        count[x-mn] -= 1
+        ndx = count[x-mn] # find the position of the x where it should be placed
+        sorted_arr[ndx] = x # place the number at the correct position
+    
+    return sorted_arr
+
+arr = list(map(int,input().split()))
+sorted_arr = count_sort(arr)
+print(sorted_arr)
