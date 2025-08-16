690f8d7fc2d6887a86fef14bc4f3a9778b2dc3bb
Nahin Munkar <nahinmunkarnm@gmail.com>
2025-08-17 00:49:20 +0600

    update binary search

--- python/Searching/binarySearch_v2.py
+++ python/Searching/binarySearch_v2.py
@@ -1,13 +1,12 @@
 def bianey_search(arr,x):
     lw,hi = 0,len(arr)-1
     if x<arr[lw] or arr[hi]<x: return False
-    mid = (lw+hi)>>1
 
     while lw<=hi:
+        mid = (lw+hi)>>1
         if mid==x: return True
         if x<mid: hi = mid-1
         else: lw = mid+1
-        mid = (lw+hi)>>1
 
 arr = [1,2,3,4,5,6,7,8,9]
 for x in [-1,5,15,6]:
